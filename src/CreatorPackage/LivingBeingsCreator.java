89005a26816b8de43b45bb5c834a714bf5cae4ef
pillanosl <141068461+pillanosl@users.noreply.github.com>
2024-03-21 00:53:28 -0500

    Update LivingBeingsCreator.java

--- src/CreatorPackage/LivingBeingsCreator.java
+++ src/CreatorPackage/LivingBeingsCreator.java
@@ -1,10 +1,35 @@
 package CreatorPackage;
 
+import BeingsOfTheEarth.EarthAnimal;
+import BeingsOfTheEarth.Human;
+import SkyBeings.SkyAnimal;
+
 public class LivingBeingsCreator implements Creator {
-    //Crear instancia de seres vivios que implemente la interfaz de creator, tanto del cielo y la tierra
-    public void createLivingBeings(String species, String origin ){
-        System.out.println("Creando ser vivo ..."+species);
-
+    public LivingBeings createLivingBeings(String species) {
+        System.out.println("------... Creating a living being ...--------");
+        if ("Human".equals(species)) {
+            return new Human("Pedro", "Colombia");
+        } else {
+            String[] ArrCountries = {"Colombia", "Paraguay", "Brazil", "Mexico", "Australia", "Peru", "Chile"};
+            if ("SkyAnimal".equals(species)) {
+                String[] ArrSkyBeings = {"Bird", "Parrot", "Macaw", "Butterfly", "Hummingbird", "Eagle", "Dragon"};
+                int i1 = (int) (Math.random() * 7);
+                String skyType = ArrSkyBeings[i1];
+                int i2 = (int) (Math.random() * 7);
+                String skyOrigin = ArrCountries[i2];
+                return new SkyAnimal(skyType, skyOrigin);
+            } else {
+                if ("EarthAnimal".equals(species)) {
+                    String[] ArrEarthBeings = {"Dog", "Cat", "Horse", "Cow", "Camel", "Kangaroo", "Elephant"};
+                    int i3 = (int) (Math.random() * 7);
+                    String skyType = ArrEarthBeings[i3];
+                    int i4 = (int) (Math.random() * 7);
+                    String skyOrigin = ArrCountries[i4];
+                    return new EarthAnimal(skyType, skyOrigin);
+                }
+            }
+        }
+        return null;
     }
-}
 
+}
