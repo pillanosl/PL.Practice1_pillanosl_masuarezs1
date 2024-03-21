f7e1d2940cbeafdab9d57383543428fb6b7057a8
pillanosl <141068461+pillanosl@users.noreply.github.com>
2024-03-21 00:53:44 -0500

    Update Main.java

--- src/CreatorPackage/Main.java
+++ src/CreatorPackage/Main.java
@@ -6,29 +6,44 @@ import SkyBeings.SkyAnimal;
 
 public class Main {
     public static void main(String[] args) {
-        Human pedro = new Human();
-        pedro.born();
-        pedro.eat();
-        pedro.grow(3);
-        pedro.fly();
-        pedro.respawn();
-        pedro.walk();
-        pedro.sing();
-        pedro.die();
-        pedro.changeDimensionChange();
 
-        System.out.println('\n');
-        EarthAnimal cat = new EarthAnimal();
-        cat.born();
-        cat.walk();
+        LivingBeingsCreator ser1 = new LivingBeingsCreator();
+        Human human1 = (Human) ser1.createLivingBeings("Human");
+        int i = (int) (Math.random() * 4);
+        System.out.println("Human called " +human1.getName()+" from "+ human1.getOrigin()+".");
+        human1.born();
+        human1.grow(i*2);
+        human1.respawn();
+        human1.eat();
+        human1.walk();
+        human1.sing();
+        human1.changeDimensionChange();
+        human1.die();
+        System.out.println("\n");
 
-        System.out.println('\n');
-        SkyAnimal loro = new SkyAnimal();
-        loro.born();
-        loro.die();
-        loro.sing();
+        LivingBeingsCreator ser2 = new LivingBeingsCreator();
+        SkyAnimal SkyAni = (SkyAnimal) ser2.createLivingBeings("SkyAnimal");
+        System.out.println("SkyAnimal type " +SkyAni.getType()+" from "+ SkyAni.getOrigin()+".");
+        SkyAni.born();
+        SkyAni.grow(i+1);
+        SkyAni.sing();
+        SkyAni.fly();
+        SkyAni.walk();
+        SkyAni.eat();
+        SkyAni.respawn();
+        SkyAni.changeDimensionChange();
+        SkyAni.die();
+        System.out.println("\n");
 
-
+        LivingBeingsCreator ser3 = new LivingBeingsCreator();
+        EarthAnimal EarthAni = (EarthAnimal) ser3.createLivingBeings("EarthAnimal");
+        System.out.println("Earth animal type " +EarthAni.getType()+" from "+EarthAni.getOrigin()+".");
+        EarthAni.born();
+        EarthAni.walk();
+        EarthAni.grow(i+2);
+        EarthAni.respawn();
+        EarthAni.eat();
+        EarthAni.die();
 
     }
 }
