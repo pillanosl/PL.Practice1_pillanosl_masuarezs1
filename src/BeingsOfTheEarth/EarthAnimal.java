447e44b036bdda338a8c3185c53b0baf805d799e
pillanosl <141068461+pillanosl@users.noreply.github.com>
2024-03-21 00:51:42 -0500

    Update EarthAnimal.java

--- src/BeingsOfTheEarth/EarthAnimal.java
+++ src/BeingsOfTheEarth/EarthAnimal.java
@@ -3,37 +3,66 @@ package BeingsOfTheEarth;
 import CreatorPackage.LivingBeings;
 
 public class EarthAnimal extends LivingBeings implements EarthBeing {
+    String type;
+    String origin;
 
+    public EarthAnimal(String type, String origin) {
+        this.type = type;
+        this.origin = origin;
+    }
+
+
+    public String getType() {
+        return type;
+    }
+
+    public String getOrigin() {
+        return origin;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public void setOrigin(String origin) {
+        this.origin = origin;
+    }
 
     @Override
     public void born() {
-        System.out.println("Hello world, I'm Kitty");
+
+        System.out.println("Hello world, I'm a earth animal!");
     }
 
     @Override
     public void grow(int years) {
-        System.out.println("I grew up" +years+" months");
+        System.out.println("The earth animal grew up " +years+ " years.");
+    }
+
+
+    @Override
+    public void eat() {
+
+        System.out.println("The earth animal is eating ñam, ñam.");
     }
 
     @Override
     public void respawn() {
-        System.out.println("I gave birth to ... kitties");
-    }
-
-    @Override
-    public void eat() {
-        System.out.println("Eating meat ñam");
+        System.out.println("The earth animal gave birth to many children.");
     }
 
     @Override
     public void die() {
-        System.out.println("Kitten died");
+
+        System.out.println("The earth animal died...");
     }
 
 
     @Override
     public void walk() {
-        System.out.println("Walking miau, miau");
-    }
-}
 
+        System.out.println("The earth animal is walking home.");
+    }
+
+
+}
